23d4c075590da1047be509bbdbd8b77267ad6835
ramakanth_rapaka8824 <ramakanth.rapaka@gmail.com>
2020-05-09 21:35:41 +0530

    registration

---
+++ client/src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+<h1>Register</h1>
+<input type="text" id="email" name="email" placeholder="EMAIL" value="" v-model="email" />
+<br>
+<input type="password" id="password" name="password" placeholder="PASSWORD" value="" v-model="password" />
+<br>
+<button type="submit" @click="register">LOGIN</button>
+</div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService';
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+     const response =  await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data);
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 2000)
+  },
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
